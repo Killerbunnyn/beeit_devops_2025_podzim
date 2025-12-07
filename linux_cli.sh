5b436a8e8a9ddbccaacd71d39d555441f070d8fe
Killerbunnyn <killerbunny@post.cz>
2025-12-07 14:41:58 +0100

    zmena v linux cli - pridani flagů

--- linux_cli.sh
+++ linux_cli.sh
@@ -1,61 +1,55 @@
-!/bin/bash
+#!/bin/bash
 
-#tyhle prikazy jsou presunty do zakladniho infa
-#echo $SHELL
-#whoami
-#cat /etc/os-release
-#printenv
-
+# --------------- PROMĚNNÉ ----------------
 LOGOVAT_DO_SOUBORU=false
 SOUBOR_LOGU="./linux_cli.log"
 
-# --------- 1. MANUÁL ---------
+# ---------------- FLAGY ------------------
+FLAG_VYPSAT_UPDATE=false
+FLAG_VYTVORIT_LINK=false
+FLAG_UPGRADE=false
+CILOVY_SOUBOR_LOGU=""
+LINK_ZDROJ=""
+LINK_CIL=""
+
+
+# --------------- 1. MANUÁL ---------------
 
 function zobrazitManual() {
     echo -e "
 =======================================================
 ----------------MANUÁL (linux_cli.sh)-----------------
 =======================================================
-----------Script pro správu systému a souborů----------
-
+-----------Script pro správu systému a souborů---------
 
 -----------------------POUŽITÍ-------------------------
 
- Pro spuštění napiš ./linux_cli.sh <příkaz z manuálu>
+ Pro spuštění napiš ./linux_cli.sh <FLAGY> [POZIČNÍ ARGUMENTY]
 
 =======================================================
 
------------------------PŘÍKAZY-------------------------
+-----------------------FLAGY---------------------------
 
-  -h, help          Zobrazí manuál
-
-  zakladni-info     Vypíše info o uživateli, shellu a OS
-  log-stdout        Nastaví výpis logů
-  log-soubor        Nastaví výpis logů do souboru '$SOUBOR_LOGU'
-
-  seznam-update     Vypíše balíčky, který čekaj na aktualizaci
-  udelat-upgrade    Provede 'apt update' a 'apt upgrade'
-
-  vytvorit-link     Vytvoří soft nebo hard link
-    Parametry: <typ: soft/hard> <zdroj> <cil>
-
-  najit-regex       Najde soubory obsahující b,e,a,e
-    Parametr: [adresar_kde_hledat] (volitelné)
-
-  samoinstalace     Vytvoří soft link na tento skript do /bin/linux_cli
+  -h            Zobrazit manuál
+  -i            Vypíše info o uživateli, shellu a OS
+  -a            Vypíše balíčky, který čekaj na aktualizaci
+  -u            Provede 'apt update' a 'apt upgrade'
+  -s            Vytvoří SOFT LINK - musí se zadat v tomhle pořadí: <zdroj> <cíl>
+  -f <soubor>   Přepne výpis logů někam
+  -x            Samoinstalace skriptu do /bin/linux_cli (samoinstalace)
 
 -------------------------------------------------------
 "
 }
 
-# --------- 2. LOGOVÁNÍ ---------
+# --------------- 2. LOGOVÁNÍ ---------------
 
 function zaloguj() {
     local zprava="[INFO] $(date '+%Y-%m-%d %H:%M:%S') - $1"
     if [ "$LOGOVAT_DO_SOUBORU" = true ]; then
         echo "$zprava" >> "$SOUBOR_LOGU"
     else
-        echo -e "\033[32m$zprava\033[0m" #pozivitni_zelena
+        echo -e "\033[32m$zprava\033[0m"
     fi
 }
 
@@ -66,28 +60,15 @@ function zalogujChybu() {
     if [ "$LOGOVAT_DO_SOUBORU" = true ]; then
         echo "$zprava" >> "$SOUBOR_LOGU"
     else
-        echo -e "\033[31m$zprava\033[0m" >&2 #negativni_cervena
+        echo -e "\033[31m$zprava\033[0m" >&2
     fi
 
     if [ ! -z "$kod_ukonceni" ]; then
-        exit "$kod_ukonceni"
+       exit "${kod_ukonceni:-1}"
     fi
 }
 
-function nastavitVystupLogu() {
-    local volba="$1"
-    if [ "$volba" == "soubor" ]; then
-        LOGOVAT_DO_SOUBORU=true
-        zaloguj "Logování přepnuto do $SOUBOR_LOGU"
-    elif [ "$volba" == "stdout" ]; then
-        LOGOVAT_DO_SOUBORU=false
-        zaloguj "Logování přepnuto na STDOUT"
-    else
-        zalogujChybu "Chyba - napiš 'soubor' nebo 'stdout'." 1
-    fi
-}
-
-# --------- 3. SYSTÉMOVÉ FUNKCE ---------
+# --------------- 3. SYSTÉMOVÉ FUNKCE ---------------
 
 function zakladniInfo() {
     zaloguj "Vypsání informací o systému:"
@@ -96,15 +77,16 @@ function zakladniInfo() {
     echo "Verze OS:"
     cat /etc/os-release
     printenv | head -n 5
+    return 0
 }
 
 function vypsatAktualizace() {
     zaloguj "Vyhledávání balíčků"
     if ! sudo apt update 2>/dev/null; then
-        zalogujChybu "Chyba"
+        zalogujChybu "Chyba při spuštění 'apt update'." 1
         return 1
     fi
-    local seznam_balicku=$(apt list --upgradable 2>/dev/null | grep -v 'hledání')
+    local seznam_balicku=$(apt list --upgradable 2>/dev/null | grep -v 'Listing...')
 
     if [ -z "$seznam_balicku" ]; then
         zaloguj "Žádný aktualizace k dispozici"
@@ -112,32 +94,27 @@ function vypsatAktualizace() {
         zaloguj "Dostupné aktualizace:"
         echo "$seznam_balicku"
     fi
+    return 0
 }
 
 function provestUpgrade() {
     zaloguj "Probíhá aktualizace"
     if sudo apt update && sudo apt upgrade -y; then
         zaloguj "Update a Upgrade hotovo."
+        return 0
     else
-        zalogujChybu "Chyba" 1
+        zalogujChybu "Chyba při aktualizaci." 1
+        return 1
     fi
 }
 
-# --------- 4. SOUBORY A LINKY ---------
+# --------------- 4. SOUBORY A LINKY ---------------
 
 function vytvoritLink() {
-    if [ "$#" -ne 3 ]; then
-        zalogujChybu "chyba mrkni na manual" 1
-    fi
-
     local typ_linku="$1"
     local zdroj="$2"
     local cil="$3"
     local prikaz="ln"
-
-    if [ ! -e "$zdroj" ]; then
-        zalogujChybu "soubor neexistuje" 1
-    fi
 
     if [ "$typ_linku" == "soft" ]; then
         prikaz="ln -s"
@@ -145,82 +122,128 @@ function vytvoritLink() {
     elif [ "$typ_linku" == "hard" ]; then
         prikaz="ln"
         zaloguj "Vytváří se HARD link"
-    else
-        zalogujChybu "Použij 'soft' nebo 'hard'" 1
     fi
 
     if $prikaz "$zdroj" "$cil"; then
         zaloguj "Link byl vytvořen"
+        return 0
     else
         zalogujChybu "Vytváření linku selhalo" 1
+        return 1
+    fi
+}
+
+function samoinstalace() {
+    zaloguj "Instaluju tvuj link do /bin/linux_cli"
+    local cesta_skriptu=$(realpath "$0")
+
+    if sudo ln -sf "$cesta_skriptu" "/bin/linux_cli"; then
+        zaloguj "Hotovo"
+        return 0
+    else
+        zalogujChybu "Nepodařilo se vytvořit link" 1
+        return 1
     fi
 }
 
 function najitRegex() {
     local adresar="${1:-.}"
-
     zaloguj "Hledám soubory v: $adresar"
-
-    # Použití find s rozšířeným regexem
     find "$adresar" -regextype posix-extended -regex '.*b.*e.*a.*e.*'
-
     if [ $? -eq 0 ]; then
         zaloguj "Hledání hotovooo."
+        return 0
     else
-        zalogujChybu "Příkaz skončil chybou."
+        zalogujChybu "Příkaz find skončil chybou." 1
+        return 1
     fi
 }
 
-function samoinstalace() {
-    zaloguj "Instaluju tvuj link do /linux_cli"
-    local cesta_skriptu=$(realpath "$0")
+# =======================================================
+# ---------- 5. MAIN kód - POUŽITÍ FLAGŮ getopts --------
+# =======================================================
 
-    if sudo ln -sf "$cesta_skriptu" "/bin/linux_cli"; then
-        zaloguj "Hotovo"
-    else
-        zalogujChybu "Nepodařilo se vytvořit link" 1
+# Cyklus pro zpracování flagů
+while getopts ":hiausxuf:" volba; do
+    case "${volba}" in
+        h)
+            zobrazitManual
+            exit 0
+            ;;
+        i)
+            zakladniInfo
+            ;;
+        a)
+            FLAG_VYPSAT_UPDATE=true
+            ;;
+        u)
+            FLAG_UPGRADE=true
+            ;;
+        s)
+            FLAG_VYTVORIT_LINK=true
+            ;;
+        x)
+            samoinstalace
+            ;;
+        f)
+            CILOVY_SOUBOR_LOGU="${OPTARG}"
+            ;;
+        \?)
+            zalogujChybu "Neplatný flag: -${OPTARG}. Použijte -h." 1
+            ;;
+        :)
+            zalogujChybu "Flag -${OPTARG} vyžaduje hodnotu." 1
+            ;;
+    esac
+done
+
+shift "$((OPTIND-1))"
+
+# --------- OŠETŘENÍ A NASTAVENÍ GLOBÁLNÍCH STAVŮ ---------
+
+# 1. flag -f (soubor logu)
+if [ ! -z "$CILOVY_SOUBOR_LOGU" ]; then
+    if [ -f "$CILOVY_SOUBOR_LOGU" ]; then
+        zaloguj "Upozornění: Soubor logu '$CILOVY_SOUBOR_LOGU' již existuje. Bude připojen."
     fi
-}
-
-# --------- 5. MAIN PRIKAZY ---------
-
-if [ $# -eq 0 ]; then
-    zalogujChybu "Žádný příkaz - mrkni na manuál"
-    zobrazitManual
-    exit 1
+    SOUBOR_LOGU="$CILOVY_SOUBOR_LOGU"
+    LOGOVAT_DO_SOUBORU=true
 fi
 
-case "$1" in
-    -h|help)
-        zobrazitManual
-        ;;
-    zakladni-info)
-        zakladniInfo
-        ;;
-    log-stdout)
-        nastavitVystupLogu "stdout"
-        ;;
-    log-soubor)
-        nastavitVystupLogu "soubor"
-        ;;
-    seznam-update)
-        vypsatAktualizace
-        ;;
-    udelat-upgrade)
-        provestUpgrade
-        ;;
-    vytvorit-link)
-        vytvoritLink "$2" "$3" "$4"
-        ;;
-    najit-regex)
-        najitRegex "$2"
-        ;;
-    samoinstalace)
-        samoinstalace
-        ;;
-    *)
-        zalogujChybu "Neznámý příkaz '$1'. Pro nápovědu použijte -h." 1
-        ;;
-esac
 
-exit 
+# 2. Zpracování zadani pro tvorbu linku s flagem -s)
+if [ "$FLAG_VYTVORIT_LINK" = true ]; then
+    if [ "$#" -lt 2 ]; then
+        zalogujChybu "Chyba: Flag -s vyžaduje dva poziční argumenty: <zdroj> <cíl_linku>." 1
+    fi
+    LINK_ZDROJ="$1"
+    LINK_CIL="$2"
+    
+    if [ -e "$LINK_CIL" ]; then
+        zalogujChybu "Link '$LINK_CIL' již existuje. Akce zrušena." 1
+    fi
+    
+    shift 2
+fi
+
+
+# 3. Update, Link, Upgrade
+
+if [ "$FLAG_VYPSAT_UPDATE" = true ]; then
+    vypsatAktualizace
+fi
+
+if [ "$FLAG_VYTVORIT_LINK" = true ]; then
+    vytvoritLink "soft" "$LINK_ZDROJ" "$LINK_CIL"
+fi
+
+if [ "$FLAG_UPGRADE" = true ]; then
+    provestUpgrade
+fi
+
+if [ "$#" -gt 0 ]; then
+    najitRegex "$1"
+fi
+
+# konec
+exit 0
